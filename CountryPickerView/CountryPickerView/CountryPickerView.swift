973266087710b70b0764ef6243bbed32292a4af1
Daniel Esteban Salinas Suárez <32717486+steve123s@users.noreply.github.com>
2019-11-15 15:52:54 -0600

    Make controller always present inside a NC

--- CountryPickerView/CountryPickerView/CountryPickerView.swift
+++ CountryPickerView/CountryPickerView/CountryPickerView.swift
@@ -137,22 +137,15 @@ public class CountryPickerView: NibView {
     public func showCountriesList(from viewController: UIViewController) {
         let countryVc = CountryPickerViewController(style: .grouped)
         countryVc.countryPickerView = self
-        if let viewController = viewController as? UINavigationController {
-            delegate?.countryPickerView(self, willShow: countryVc)
-            viewController.pushViewController(countryVc, animated: true) {
-                self.delegate?.countryPickerView(self, didShow: countryVc)
-            }
-        } else {
-            let navigationVC = UINavigationController(rootViewController: countryVc)
-            
-            navigationVC.navigationBar.prefersLargeTitles = true
-            navigationVC.navigationItem.largeTitleDisplayMode = .always
-            navigationVC.navigationBar.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
-            
-            delegate?.countryPickerView(self, willShow: countryVc)
-            viewController.present(navigationVC, animated: true) {
-                self.delegate?.countryPickerView(self, didShow: countryVc)
-            }
+        let navigationVC = UINavigationController(rootViewController: countryVc)
+        
+        navigationVC.navigationBar.prefersLargeTitles = true
+        navigationVC.navigationItem.largeTitleDisplayMode = .always
+        navigationVC.navigationBar.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        
+        delegate?.countryPickerView(self, willShow: countryVc)
+        viewController.present(navigationVC, animated: true) {
+            self.delegate?.countryPickerView(self, didShow: countryVc)
         }
     }
     
